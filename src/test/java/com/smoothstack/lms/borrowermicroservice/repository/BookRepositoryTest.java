6d76586e5d7b6a56b1b2562139916f26a08027d6
Krissada Jindanupajit <krissada.jindanupajit@smoothstack.com>
2020-02-18 15:25:33 -0500

    Test

--- src/test/java/com/smoothstack/lms/borrowermicroservice/repository/BookRepositoryTest.java
+++ src/test/java/com/smoothstack/lms/borrowermicroservice/repository/BookRepositoryTest.java
@@ -10,13 +10,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BookRepositoryTest {
 
-    @Test
-    void cacheOfIdMustCacheABook() {
-        CrudRepository bookRepository = CrudRepositoryFactory.getRepository(Book.class);
 
-        CachedOne<Book> aBook = bookRepository.cacheOfId(10);
-
-        assertEquals(null, aBook.get().orElse(new Book()), "Not A Book");
-    }
 
 }
